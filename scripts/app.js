6e04af59ee3f14d36a1f52eb8ddef3cfef3ad413
Rachelle <archaurore@gmail.com>
2016-11-25 17:49:51 -0500

    building state object with old and new regions

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,28 @@
-$(document).ready(function() {
-	$('.js-search-btn').click(function(e) {
-		e.preventDefault();
-		console.log('clicked');
-	});
-});
+'use strict'
+
+var state = {
+	regions : ["auvergne-rhone-alpes", "bourgogne-franche-comte", "bretagne","centre-val-de-loire", 
+	"corse", "grand-est", "guadeloupe", "guyane", "hauts-de-france", "ile-de-france", "la-reunion", 
+	"martinique", "mayotte", "normandie", "nouvelle-aquitaine", "occitanie", "pays-de-la-loire", 
+	"provence-alpes-cote-dazure"],
+	oldRegions : {
+		"alsace" : "grand-est",
+		"aquitaine" : "nouvelle-aquitaine",
+		"auvergne" : "auvergne-rhone-alpes",
+		"basse-normandie" : "normandie",
+		"bourgogne" : "bourgogne-franche-comte",
+		"centre" : "centre-val-de-loire",
+		"champagne-ardenne" : "grand-est",
+		"franche-comte" : "bourgogne-franche-comte",
+		"haute-normandie" : "normandie",
+		"languedoc-rousillon" : "occitanie",
+		"limousin" : "nouvelle-aquitaine",
+		"lorraine" : "grand-est",
+		"midi-pyrenees" : "occitanie",
+		"nord-pas-de-calais" : "hauts-de-france",
+		"picardie" : "hauts-de-france",
+		"poitou-charentes" : "nouvelle-aquitaine",
+		"rhone-alpes" : "auvergne-rhone-alpes",
+
+	}
+};
